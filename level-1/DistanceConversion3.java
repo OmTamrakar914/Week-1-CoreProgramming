4b112bf491fde0c16d269988510492796fec1ddd
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-11 15:10:23 +0530

    Distance Conversion

---
+++ level-1/DistanceConversion3.java
@@ -0,0 +1,17 @@
+// Converting kilometers to miles
+
+import java.util.*;
+
+public class DistanceConversion3{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		
+		// calculating equivalent miles to kilometers
+		// 1 km = 1.6 miles
+		
+		double kilometers = 10.8;
+        double miles = kilometers / 1.6;
+
+    System.out.println("The Distance "+10.6+" km in miles is "+miles);		
+	}
+}
