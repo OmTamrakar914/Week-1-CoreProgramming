22496766983dc6fdb02d5d851ab051b854ecb66a
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-01-11 15:09:21 +0530

    Average of Sam

---
+++ level-1/AverageOfSam2.java
@@ -0,0 +1,20 @@
+// Sam's Average percentage 
+
+import java.util.*;
+
+public class AvergeOfSam2{
+	public static void main(String []args){
+		
+		
+		//Sam's mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100.
+
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+		//Calculating the average
+        double average = (maths + physics + chemistry)/3;
+        
+		
+        System.out.print("Sam's average mark in PCM is "+average);  		
+	}
+}
